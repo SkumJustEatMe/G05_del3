36fd304ec71d63f2b886b76598d6ac00e8d60ff5
Anders Nørgaard <ansi1806@gmail.com>
2022-11-09 12:53:02 +0100

    added deck-class, and updated other subclasses

--- src/main/Game/FreeFieldCard.java
+++ src/main/Game/FreeFieldCard.java
@@ -1,19 +1,24 @@
 package Game;
 public class FreeFieldCard extends ChanceCard{
     String text;
-    int value;
+    int value, value2, value3;
 
-    public FreeFieldCard(String text, int value){
+    public FreeFieldCard(String text, int value, int value2, int value3){
         this.text = text;
         this.value = value;
     }
     public void execute(Player p){
         int spot;
         p.curSqr = spot;
-        if (p.curSqr = value){
+
+        if (p.curSqr == value || p.curSqr == value2||p.curSqr == value3){
             p.curSqr +=  1;
-        }else{
-            p.curSqr = value;
+        }else if(p.curSqr > value && p.curSqr < value2){
+            p.curSqr = value2;
+        } else if(p.curSqr > value2 && p.curSqr < value3){
+        p.curSqr = value3;
+        } else {
+        p.curSqr = value;
         }
         if(spot > p.curSqr){
             p.changeBalance(200);
