f008792422dc6e9cee2c69e3623a7a9e0f009922
Emilkroldrup <Emil@ak-es.dk>
2022-11-11 00:07:29 +0100

    added 23 cases for each board field

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,2 +1,80 @@
 public class Board {
+
+
+    public static String getFieldname(int roll){
+        String name = null;
+        switch (roll) {
+
+            case 2:
+                name = "2. test: ";
+                break;
+            case 3:
+                name = "3. ";
+                break;
+            case 4:
+                name = "4. ";
+                break;
+            case 5:
+                name = "5. ";
+                break;
+            case 6:
+                name = "6. ";
+                break;
+            case 7:
+                name = "7. ";
+                break;
+            case 8:
+                name = "8. ";
+                break;
+            case 9:
+                name = "9. ";
+                break;
+            case 10:
+                name = "10.";
+                break;
+            case 11:
+                name = "11.";
+                break;
+            case 12:
+                name = "12.";
+                break;
+            case 13:
+                name = "13. ";
+                break;
+            case 14:
+                name = "14.  ";
+                break;
+            case 15:
+                name = "15.  ";
+                break;
+            case 16:
+                name = "16.  ";
+                break;
+            case 17:
+                name = "17.  ";
+                break;
+            case 18:
+                name = "18.  ";
+                break;
+            case 19:
+                name = "19.  ";
+                break;
+            case 20:
+                name = "20.  ";
+                break;
+            case 21:
+                name = "21. ";
+                break;
+            case 22:
+                name = "22. ";
+                break;
+            case 23:
+                name = "23. ";
+                break;
+        }
+
+
+
+    }
+
 }
