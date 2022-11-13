47d5e8d72242269bd1de393e46f25a4f0e802254
Emilkroldrup <113909369+Emilkroldrup@users.noreply.github.com>
2022-11-13 15:08:39 +0100

    lagt navne ind

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -5,76 +5,162 @@ public class Board {
         String name = null;
         switch (roll) {
 
+            case 1:
+                name = "Starting field";
+                break;
             case 2:
-                name = "2. test: ";
+                name = "Corner Kitchen Burgerbar ";
                 break;
             case 3:
-                name = "3. ";
+                name = "Pizza House;
                 break;
             case 4:
-                name = "4. ";
+                name = "Chance card";
                 break;
             case 5:
-                name = "5. ";
+                name = "Candy store";
                 break;
             case 6:
-                name = "6. ";
+                name = "Icecream store";
                 break;
             case 7:
-                name = "7. ";
+                name = "Prison visit";
                 break;
             case 8:
-                name = "8. ";
+                name = "Museum";
                 break;
             case 9:
-                name = "9. ";
+                name = "Liberay";
                 break;
             case 10:
-                name = "10.";
+                name = "Chance card";
                 break;
             case 11:
-                name = "11.";
+                name = "Skating park";
                 break;
             case 12:
-                name = "12.";
+                name = "Swimming pool";
                 break;
             case 13:
-                name = "13. ";
+                name = "Free parking spot";
                 break;
             case 14:
-                name = "14.  ";
+                name = "Playing hall";
                 break;
             case 15:
-                name = "15.  ";
+                name = "Cinema";
                 break;
             case 16:
-                name = "16.  ";
+                name = "Chance card";
                 break;
             case 17:
-                name = "17.  ";
+                name = "Toy store";
                 break;
             case 18:
-                name = "18.  ";
+                name = "Pet store";
                 break;
             case 19:
-                name = "19.  ";
+                name = "Go to jail";
                 break;
             case 20:
-                name = "20.  ";
+                name = "Bowling allay";
                 break;
             case 21:
-                name = "21. ";
+                name = "Zoo";
                 break;
             case 22:
-                name = "22. ";
+                name = "Chance card";
                 break;
             case 23:
-                name = "23. ";
+                name = "Waterpark";
+                break;
+            case 24:
+                name = "The beach promenade";
                 break;
         }
+        return name;
 
 
 
     }
+    public static String getFieldDescription(int roll) {
+        String descriptoion = null;
+        Switch (roll) {
+            case 1:
+                descriptoion = "Starting field";
+                break;
+            case 2:
+                descriptoion = "Corner Kitchen Burgerbar ";
+                break;
+            case 3:
+                descriptoion = "Pizza House;
+                break;
+            case 4:
+                descriptoion = "Chance card";
+                break;
+            case 5:
+                descriptoion = "Candy store";
+                break;
+            case 6:
+                descriptoion = "Icecream store";
+                break;
+            case 7:
+                descriptoion = "Prison visit";
+                break;
+            case 8:
+                descriptoion = "Museum";
+                break;
+            case 9:
+                descriptoion = "Liberay";
+                break;
+            case 10:
+                descriptoion = "Chance card";
+                break;
+            case 11:
+                descriptoion = "Skating park";
+                break;
+            case 12:
+                descriptoion = "Swimming pool";
+                break;
+            case 13:
+                descriptoion = "Free parking spot";
+                break;
+            case 14:
+                descriptoion = "Playing hall";
+                break;
+            case 15:
+                name = "Cinema";
+                break;
+            case 16:
+                name = "Chance card";
+                break;
+            case 17:
+                name = "Toy store";
+                break;
+            case 18:
+                name = "Pet store";
+                break;
+            case 19:
+                name = "Go to jail";
+                break;
+            case 20:
+                name = "Bowling allay";
+                break;
+            case 21:
+                name = "Zoo";
+                break;
+            case 22:
+                name = "Chance card";
+                break;
+            case 23:
+                name = "Waterpark";
+                break;
+            case 24:
+                name = "The beach promenade";
+                break;
+
+        }
+    }
+
 
 }
