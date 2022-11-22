37968023aa4e6515a70489a6ffd4f7638586c923
SkumJustEatMe <s214662@dtu.dk>
2022-11-22 20:28:59 +0100

    Lavet ny branch, så vi kan lave JUnit test

--- src/main/java/game/main.java
+++ src/main/java/game/main.java
@@ -4,6 +4,7 @@ public class main {
     public static void main(String[] args)
     {
         GameController gameController = new GameController();
+        
         gameController.run();
     }
 }
